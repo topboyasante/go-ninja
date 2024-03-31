efaefd596e5ce0e15104b6ad9c6384d3f41d3cd3
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-03-31 18:06:15 +0000

    course complete

--- project/bill.go
+++ project/bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -12,7 +15,7 @@ type bill struct {
 func generateNewBill(name string) bill {
 	new_bill := bill{
 		name:  name,
-		items: map[string]float64{"pie": 5.99, "eggs": 5.99},
+		items: map[string]float64{},
 		tip:   0,
 	}
 
@@ -20,8 +23,10 @@ func generateNewBill(name string) bill {
 }
 
 // reciever functions: simple
-func (b bill) format() string {
-	header := "Bill Breakdown \n" 
+
+// Get the breakdown of your bill
+func (b *bill) format() string {
+	header := "Bill Breakdown \n"
 	var total float64
 
 	for key, value := range b.items {
@@ -29,8 +34,35 @@ func (b bill) format() string {
 		total += value
 	}
 
-	header += fmt.Sprintf("%-25v ...$%0.2f", "Total:", total)
+	header += fmt.Sprintf("%-25v ...$%0.2f \n", "tip:", b.tip)
+
+	header += fmt.Sprintf("%-25v ...$%0.2f", "Total:", total+b.tip)
 	return header
 }
 
 //%-25v make that variable 25 characters long to the right. make it positive and the spaces are on the left
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+	// or you can do this: (*b).tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
+//for structs pointers are automatically dereferenced
+
+func (b *bill) saveBill() {
+	data := []byte(b.format())
+
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+
+	if err != nil {
+		{
+			panic(err)
+		}
+	}
+
+	fmt.Println("bill was saved.")
+}
--- project/main.go
+++ project/main.go
@@ -1,10 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getInput(r *bufio.Reader, prompt string) (s string, e error) {
+	fmt.Print(prompt)
+	input, error := r.ReadString('\n')
+	return strings.TrimSpace(input), error
+}
+
+func createBill() bill {
+	//Readers are used to get user input through the terminal.
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput(reader, "Create a New Bill Name:")
+
+	b := generateNewBill(name)
+	fmt.Printf("Created bill %v \n", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput(reader, "Choose an option: \n A - Add Item \n S - Save Bill \n T - Add Tip \n")
+	fmt.Println(opt)
+
+	switch opt {
+	case "a":
+		name, _ := getInput(reader, "What is the item name? \n")
+		price, _ := getInput(reader, "How much is it? \n")
+
+		//convert strings to float
+		p, err := strconv.ParseFloat(price, 64)
+
+		if err != nil {
+			fmt.Println("Invalid Price value")
+			promptOptions(b)
+		}
+
+		b.addItem(name, p)
+
+		fmt.Println("Item added!")
+
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput(reader, "How much do you want to tip?")
+
+		p, err := strconv.ParseFloat(tip, 64)
+
+		if err != nil {
+			fmt.Println("Invalid Price value")
+			promptOptions(b)
+		}
+
+		b.updateTip(p)
+
+		fmt.Println("Tip Updated!")
+
+		promptOptions(b)
+	case "s":
+		b.saveBill()
+	default:
+		fmt.Println("Invalid Input")
+		promptOptions(b) //looks like recursion
+	}
+}
 
 func main() {
-	myBill := generateNewBill("Hitler's Bill")
-
-	fmt.Println(myBill.format())
-
+	mybill := createBill()
+	promptOptions(mybill)
+	fmt.Println(mybill)
 }
